a6173c418ade33d12ed4c2f5824820698fe254b5
Xehanor <donatiendeffobis@yahoo.fr>
2017-10-03 17:56:49 +0200

    Second commit

--- TPProjetNodeJS/tp/app.js
+++ TPProjetNodeJS/tp/app.js
@@ -1,4 +1,6 @@
 'use strict';
+//initialisation fs
+var fs=require('fs');
 //initialisation de js
 var express=require("express"); 
 var app=express();
@@ -8,13 +10,13 @@ var CONFIG = require("./config.json");
 process.env.CONFIG = JSON.stringify(CONFIG);
 
 var server = http.createServer(app);
-server.listen(CONFIG.port,function(){
+server.listen(CONFIG.port); /*,function(){
 	var host = this.adress().address;
 	var port = this.adress().port;
 	console.log(" Serveur ecoute à l'adresse \""+host+":"+port+"\"");
-})
+})*/
 
-/*// Route par défault
+// Route par défault
 var defaultRoute = require("./app/routes/default.route.js");
 app.use(defaultRoute);
 
@@ -23,54 +25,64 @@ app.use(defaultRoute);
 app.get("/", function(request, response) {
 	response.send("It works !");
 });
-*/
+
 // Send aguero.. on index.html
 var path = require ("path");
 
 app.use("/index.html", express.static(path.join(__dirname, "public")));
 
-//initialisation fs
-
-var fs=require('fs');
 
 //creation de loadpres
-app.use("/loadPres",function(request, response, cb) {
+app.use("/loadPres",function(request, response,cb) {
 	
-	fs.readdir(CONFIG.presentationDirectory,function(err,files)
+	var directory= CONFIG.presentationDirectory;
+	var maMap= new Map();
+	fs.readdir(directory,function(err,files)
 	{
-		if(!!err)
-		{
-			console.error(err);
-			return;
-		}
-			var json={};
-			files.forEach(function(err,file){
-				if(!!err)
-				{
-					console.error(err);
-					return;
-				}
-				if(path.extname(file)==".json")
-				{
-				var cle=""
-				var valeur="";
-					JSON.parse(file,(key,value)=>{
-						if(key=="id")
-							cle=value;
-						if(key=="slidArray")
-							valeur=value;
-					});
-				json[cle]=valeur;
+		var longueur=files.length;
 
+		if(!!err)
+		return console.error(err);
+			var i=0;	
+			files.forEach(function(file){	
+
+				var fileext=path.extname(file);
+				if(fileext==".json")
+				{
+				
+				fs.readFile((directory+"/"+file),function(err,data){
+					if(!!err)
+	    				return console.error(err);
+					var parsed = JSON.parse(data);
+					var cle= parsed.id;
+					var valeur=parsed.slidArray;
+					maMap[cle]=valeur;
+					i=i+1;
+					console.log(longueur);
+					console.log(i);
+					if(longueur==i)
+					{
+						response.send(maMap);
+						cb(maMap);
+					}
+
+					
+					/*response.send(maMap);
+					response.end();*/
+
+						});
+					
 				
 				}
-				cb(json);
-
+				else{
+						i=i+1;
+				}
+			
 		});
-		
+	
 
 
-	});*/
+	});
 
 });
 //to create fonction model question 14
